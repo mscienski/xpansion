59308bcedfdda90f3ae409f2ae8f3e74553c7793
mscienski <mscienski@gmail.com>
2016-01-04 21:28:20 -0700

    try to get output

--- xpansion.py
+++ xpansion.py
@@ -1,8 +1,1 @@
-import sys
-import BaseHTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-
-
-HandlerClass = SimpleHTTPRequestHandler
-ServerClass  = BaseHTTPServer.HTTPServer
-Protocol     = "HTTP/1.1"
+print "testing"
