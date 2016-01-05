f5429493fafb74f0b117359b206b760a72244702
mscienski <mscienski@gmail.com>
2016-01-04 22:32:30 -0700

    test requests

--- xpansion.py
+++ xpansion.py
@@ -1,7 +1,29 @@
+from requests import Request, Session
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+WIKIURI = 'http://en.wikipedia.org/w/api.php'
+
 def app(environ, start_response):
       data = "Hello, World!\n"
       start_response("200 OK", [
           ("Content-Type", "text/plain"),
           ("Content-Length", str(len(data)))
       ])
-      return iter([data])
+      stuff = find_wiki_articles('pas')
+      return iter(stuff)
+
+def find_wiki_articles(term):
+    payload = {
+        'action': 'query',
+        'titles': term
+    }
+
+    s = Session()
+    req = Request('GET', WIKIURI,
+        params=payload,
+        headers=header
+    )
+    prepped = req.prepare()
+
+    return prepped.headers
