c8f9888dfbd8c987b266a70710b647bf0abef3ac
mscienski <mscienski@gmail.com>
2016-01-04 21:14:32 -0700

    initialize

---
+++ xpansion.py
@@ -0,0 +1,8 @@
+import sys
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+
+HandlerClass = SimpleHTTPRequestHandler
+ServerClass  = BaseHTTPServer.HTTPServer
+Protocol     = "HTTP/1.1"
